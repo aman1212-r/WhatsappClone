ee6ec2de9f9ff9e038566059b7f61087e915f69a
aman1212-r <aman1212rana@gmail.com>
2026-05-24 08:50:56 +0530

    gradel update

--- app/src/main/java/com/example/whatsappclone/homescreen/ChatDesign.kt
+++ app/src/main/java/com/example/whatsappclone/homescreen/ChatDesign.kt
@@ -1,7 +1,15 @@
 package com.example.whatsappclone.homescreen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,6 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.whatsappclone.R
 import com.example.whatsappclone.chat_box.ChatDesignModel
+
 @Composable
 
 
